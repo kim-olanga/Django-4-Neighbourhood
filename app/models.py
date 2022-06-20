1fe70a524800b4cc9112ac72462a29070ebe38a4
Kimberly <kimolanga123@gmail.com>
2022-06-20 15:19:38 +0300

    create a models method for posts

--- app/models.py
+++ app/models.py
@@ -68,3 +68,11 @@ class Business(models.Model):
     def hood_biz(cls, id):
         hoodbiznas = Business.objects.filter(neighbourhood = id)
         return hoodbiznas
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default = '')
+    date = models.DateField(auto_now_add=True)
+    neighbourhood = models.ForeignKey("Neighbourhood",on_delete=models.CASCADE, default='', null=True, blank=True)
+    
