6d0bec80ae3062b12fb84baadf7b71a0a318ba42
Kimberly <kimolanga123@gmail.com>
2022-06-20 14:22:54 +0300

    create a template for updating users profile

--- app/views.py
+++ app/views.py
@@ -58,4 +58,13 @@ def update_profile(request):
             profile_form.save()
 
             return redirect('profile')
-    return render(request,'update_profile.html')
+
+        else:
+            user_form = UserUpdateForm(instance=request.user)
+            profile_form = ProfileUpdateForm(instance=request.user)
+
+            context = {
+                'user_form': user_form,
+                'profile_form': profile_form
+            }
+    return render(request,'update_profile.html',context)
