202522d678ce4a296792661a7d6680f33d7c6019
Kimberly <kimolanga123@gmail.com>
2022-06-20 15:08:27 +0300

    create update path in the urls for the app

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from . import views as user_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('update_profile/', user_views.update_profile, name='update_profile'),
 ]
