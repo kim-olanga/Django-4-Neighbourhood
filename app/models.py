f7be7a297efcfc1f8c1f3eb03971ad7d113cd094
Kimberly <kimolanga123@gmail.com>
2022-06-20 15:17:29 +0300

    create models for the business

--- app/models.py
+++ app/models.py
@@ -47,3 +47,24 @@ class Neighbourhood(models.Model):
 
     def delete_neighborhood(self):
         self.delete()
+
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default = '')
+    email = models.CharField(max_length=100, default = '')
+    neighbourhood = models.ForeignKey("Neighbourhood",on_delete=models.CASCADE, default='', null=True, blank=True)
+    description = models.TextField( default = '')
+
+    def __str__(self):
+        return f'{self.name} business'
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def hood_biz(cls, id):
+        hoodbiznas = Business.objects.filter(neighbourhood = id)
+        return hoodbiznas
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from . import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('update_profile/', user_views.update_profile, name='update_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
